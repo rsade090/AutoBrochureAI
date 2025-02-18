d0c92f5bce89e90b18481b60be65e065018c7223
r.sadeghian <116025353+rsade090@users.noreply.github.com>
2025-02-18 16:48:38 -0500

    Update main.py

--- main.py
+++ main.py
@@ -6,8 +6,9 @@ from dotenv import load_dotenv
 from bs4 import BeautifulSoup
 from IPython.display import Markdown, display, update_display
 from openai import OpenAI 
-from scraper import Website
+from Website import Website
 import yaml
+import utils as util
 
 with open("config.yaml", "r") as file:
     config = yaml.safe_load(file)
@@ -24,54 +25,9 @@ else:
 MODEL = 'gpt-4o-mini'
 openai = OpenAI()
 
-
-
-
 link_system_prompt = config["link_system_prompt"]
-def get_links_user_prompt(website):
-    user_prompt = f"Here is the list of links on the website of {website.url} - "
-    user_prompt += "please decide which of these are relevant web links for a brochure about the company, respond with the full https URL in JSON format. \
-Do not include Terms of Service, Privacy, email links.\n"
-    user_prompt += "Links (some might be relative links):\n"
-    user_prompt += "\n".join(website.links)
-    return user_prompt
-
-
-
-
-def get_links(url):
-    website = Website(url)
-    response = openai.chat.completions.create(
-        model=MODEL,
-        messages=[
-            {"role": "system", "content": link_system_prompt},
-            {"role": "user", "content": get_links_user_prompt(website)}
-      ],
-        response_format={"type": "json_object"}
-    )
-    result = response.choices[0].message.content
-    return json.loads(result)
-
-
-def get_all_details(url):
-    result = "Landing page:\n"
-    result += Website(url).get_contents()
-    links = get_links(url)
-    print("Found links:", links)
-    for link in links["links"]:
-        result += f"\n\n{link['type']}\n"
-        result += Website(link["url"]).get_contents()
-    return result
-
-
 
 system_prompt = config["system_prompt"]
-def get_brochure_user_prompt(company_name, url):
-    user_prompt = f"You are looking at a company called: {company_name}\n"
-    user_prompt += f"Here are the contents of its landing page and other relevant pages; use this information to build a short brochure of the company in markdown.\n"
-    user_prompt += get_all_details(url)
-    user_prompt = user_prompt[:5_000] # Truncate if more than 5,000 characters
-    return user_prompt
 
 
 
@@ -87,13 +43,12 @@ def create_brochure(company_name, url):
     display(Markdown(result)
 
 
-
 def stream_brochure(company_name, url):
     stream = openai.chat.completions.create(
         model=MODEL,
         messages=[
             {"role": "system", "content": system_prompt},
-            {"role": "user", "content": get_brochure_user_prompt(company_name, url)}
+            {"role": "user", "content": util.get_brochure_user_prompt(company_name, url)}
           ],
         stream=True
     )
