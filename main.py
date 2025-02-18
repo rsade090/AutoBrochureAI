6c9594168a1739959883454a85c1b2c50e0e6dcf
r.sadeghian <116025353+rsade090@users.noreply.github.com>
2025-02-18 15:31:50 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,22 @@
+import os
+import requests
+import json
+from typing import List
+from dotenv import load_dotenv
+from bs4 import BeautifulSoup
+from IPython.display import Markdown, display, update_display
+from openai import OpenAI 
+
+
+
+load_dotenv(override=True)
+api_key = os.getenv('OPENAI_API_KEY')
+
+if api_key and api_key.startswith('sk-proj-') and len(api_key)>10:
+    print("API key looks good so far")
+else:
+    print("There might be a problem with your API key? Please visit the troubleshooting notebook!")
+    
+MODEL = 'gpt-4o-mini'
+openai = OpenAI()
+
